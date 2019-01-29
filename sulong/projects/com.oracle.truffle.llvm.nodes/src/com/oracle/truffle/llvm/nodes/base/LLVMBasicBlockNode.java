55f48c9ab8b17371a819b7275c1540cc4c222ca3
Jacob Kreindl <jacob.kreindl@jku.at>
2019-01-29 15:45:56 +0100

    Enable Instrumentation of LLVMBasicBlockNode

--- sulong/projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/base/LLVMBasicBlockNode.java
+++ sulong/projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/base/LLVMBasicBlockNode.java
@@ -33,6 +33,8 @@ import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.instrumentation.GenerateWrapper;
+import com.oracle.truffle.api.instrumentation.ProbeNode;
 import com.oracle.truffle.api.instrumentation.Tag;
 import com.oracle.truffle.api.nodes.ControlFlowException;
 import com.oracle.truffle.api.nodes.ExplodeLoop;
@@ -55,7 +57,8 @@ import com.oracle.truffle.llvm.runtime.options.SulongEngineOption;
  *
  * @see <a href="http://llvm.org/docs/LangRef.html#functions">basic blocks in LLVM IR</a>
  */
-public class LLVMBasicBlockNode extends LLVMStatementNode {
+@GenerateWrapper
+public abstract class LLVMBasicBlockNode extends LLVMStatementNode {
 
     public static final int RETURN_FROM_FUNCTION = -1;
 
@@ -68,44 +71,30 @@ public class LLVMBasicBlockNode extends LLVMStatementNode {
         }
     }
 
-    @Children private final LLVMStatementNode[] statements;
-    @Child public LLVMControlFlowNode termInstruction;
-
     private final int blockId;
     private final String blockName;
 
-    private final BranchProfile controlFlowExceptionProfile = BranchProfile.create();
-    private final BranchProfile blockEntered = BranchProfile.create();
-
-    @CompilationFinal(dimensions = 1) private final long[] successorExecutionCount;
-
-    public LLVMBasicBlockNode(LLVMStatementNode[] statements, LLVMControlFlowNode termInstruction, int blockId, String blockName) {
-        this.statements = statements;
-        this.termInstruction = termInstruction;
+    public LLVMBasicBlockNode(int blockId, String blockName) {
         this.blockId = blockId;
         this.blockName = blockName;
-        successorExecutionCount = termInstruction.needsBranchProfiling() ? new long[termInstruction.getSuccessorCount()] : null;
     }
 
-    public LLVMBasicBlockNode initialize() {
-        // this block is already initialized
-        return this;
+    protected LLVMBasicBlockNode(LLVMBasicBlockNode other) {
+        this.blockId = other.blockId;
+        this.blockName = other.blockName;
     }
 
     @Override
-    @ExplodeLoop
-    public void execute(VirtualFrame frame) {
-        blockEntered.enter();
-        for (int i = 0; i < statements.length; i++) {
-            LLVMStatementNode statement = statements[i];
-            try {
-                statement.execute(frame);
-            } catch (ControlFlowException e) {
-                controlFlowExceptionProfile.enter();
-                throw e;
-            }
-        }
+    public WrapperNode createWrapper(ProbeNode probeNode) {
+        return new LLVMBasicBlockNodeWrapper(this, this, probeNode);
     }
+
+    public abstract LLVMBasicBlockNode initialize();
+
+    @Override
+    public abstract void execute(VirtualFrame frame);
+
+    public abstract LLVMControlFlowNode getTerminatingInstruction();
 
     public int getBlockId() {
         return blockId;
@@ -126,60 +115,15 @@ public class LLVMBasicBlockNode extends LLVMStatementNode {
         return String.format("id: %d name: %s", blockId, blockName == null ? "N/A" : blockName);
     }
 
-    @Override
-    public String toString() {
-        CompilerAsserts.neverPartOfCompilation();
-        return String.format("basic block %s (#statements: %s, terminating instruction: %s)", blockId, statements.length, termInstruction);
-    }
-
     /**
      * Gets the branch probability of the given successor.
      *
      * @param successorIndex
      * @return the probability between 0 and 1
      */
-    @ExplodeLoop
-    public double getBranchProbability(int successorIndex) {
-        assert termInstruction.needsBranchProfiling();
-        double successorBranchProbability;
+    public abstract double getBranchProbability(int successorIndex);
 
-        /*
-         * It is possible to get race conditions (compiler and AST interpreter thread). This avoids
-         * a probability > 1.
-         *
-         * We make sure that we read each element only once. We also make sure that the compiler
-         * reduces the conditions to constants.
-         */
-        long succCount = 0;
-        long totalExecutionCount = 0;
-        for (int i = 0; i < successorExecutionCount.length; i++) {
-            long v = successorExecutionCount[i];
-            if (successorIndex == i) {
-                succCount = v;
-            }
-            totalExecutionCount += v;
-        }
-        if (succCount == 0) {
-            successorBranchProbability = 0;
-        } else {
-            assert totalExecutionCount > 0;
-            successorBranchProbability = (double) succCount / totalExecutionCount;
-        }
-        assert !Double.isNaN(successorBranchProbability) && successorBranchProbability >= 0 && successorBranchProbability <= 1;
-        return successorBranchProbability;
-    }
-
-    public void increaseBranchProbability(int successorIndex) {
-        CompilerAsserts.neverPartOfCompilation();
-        if (termInstruction.needsBranchProfiling()) {
-            incrementCountAtIndex(successorIndex);
-        }
-    }
-
-    private void incrementCountAtIndex(int successorIndex) {
-        assert termInstruction.needsBranchProfiling();
-        successorExecutionCount[successorIndex]++;
-    }
+    public abstract void increaseBranchProbability(int successorIndex);
 
     @Override
     public boolean hasTag(Class<? extends Tag> tag) {
@@ -191,23 +135,153 @@ public class LLVMBasicBlockNode extends LLVMStatementNode {
         return new LLVMNodeObject(new String[]{LLVMNodeObjects.KEY_BLOCK_ID}, new Object[]{blockName});
     }
 
+    @Override
+    public String toString() {
+        CompilerAsserts.neverPartOfCompilation();
+        return String.format("basic block %s", getBlockId());
+    }
+
+    private static final class InitializedBlock extends LLVMBasicBlockNode {
+
+        private final BranchProfile controlFlowExceptionProfile = BranchProfile.create();
+        private final BranchProfile blockEntered = BranchProfile.create();
+
+        @CompilationFinal(dimensions = 1) private final long[] successorExecutionCount;
+
+        @Children private final LLVMStatementNode[] statements;
+        @Child public LLVMControlFlowNode termInstruction;
+
+        public InitializedBlock(LLVMStatementNode[] statements, LLVMControlFlowNode termInstruction, int blockId, String blockName) {
+            super(blockId, blockName);
+            this.successorExecutionCount = termInstruction.needsBranchProfiling() ? new long[termInstruction.getSuccessorCount()] : null;
+            this.statements = statements;
+            this.termInstruction = termInstruction;
+        }
+
+        @Override
+        public LLVMBasicBlockNode initialize() {
+            // this block is already initialized
+            return this;
+        }
+
+        @Override
+        @ExplodeLoop
+        public void execute(VirtualFrame frame) {
+            blockEntered.enter();
+            for (int i = 0; i < statements.length; i++) {
+                LLVMStatementNode statement = statements[i];
+                try {
+                    statement.execute(frame);
+                } catch (ControlFlowException e) {
+                    controlFlowExceptionProfile.enter();
+                    throw e;
+                }
+            }
+        }
+
+        @Override
+        public LLVMControlFlowNode getTerminatingInstruction() {
+            return termInstruction;
+        }
+
+        @Override
+        @ExplodeLoop
+        public double getBranchProbability(int successorIndex) {
+            assert termInstruction.needsBranchProfiling();
+            double successorBranchProbability;
+
+            /*
+             * It is possible to get race conditions (compiler and AST interpreter thread). This
+             * avoids a probability > 1.
+             *
+             * We make sure that we read each element only once. We also make sure that the compiler
+             * reduces the conditions to constants.
+             */
+            long succCount = 0;
+            long totalExecutionCount = 0;
+            for (int i = 0; i < successorExecutionCount.length; i++) {
+                long v = successorExecutionCount[i];
+                if (successorIndex == i) {
+                    succCount = v;
+                }
+                totalExecutionCount += v;
+            }
+            if (succCount == 0) {
+                successorBranchProbability = 0;
+            } else {
+                assert totalExecutionCount > 0;
+                successorBranchProbability = (double) succCount / totalExecutionCount;
+            }
+            assert !Double.isNaN(successorBranchProbability) && successorBranchProbability >= 0 && successorBranchProbability <= 1;
+            return successorBranchProbability;
+        }
+
+        @Override
+        public void increaseBranchProbability(int successorIndex) {
+            CompilerAsserts.neverPartOfCompilation();
+            if (termInstruction.needsBranchProfiling()) {
+                incrementCountAtIndex(successorIndex);
+            }
+        }
+
+        private void incrementCountAtIndex(int successorIndex) {
+            assert termInstruction.needsBranchProfiling();
+            successorExecutionCount[successorIndex]++;
+        }
+
+        @Override
+        public String toString() {
+            CompilerAsserts.neverPartOfCompilation();
+            return String.format("basic block %s (#statements: %s, terminating instruction: %s)", getBlockId(), statements.length, termInstruction);
+        }
+    }
+
     private static final class LazyBlock extends LLVMBasicBlockNode {
 
         // explicitly not an @Child to prevent Truffle from inlining the node and thereby causing an
         // unnecessarily large AST
-        private final LLVMBasicBlockNode materializedBlock;
+        @CompilationFinal(dimensions = 1) private final LLVMStatementNode[] statements;
+        private final LLVMControlFlowNode termInstruction;
 
-        private LazyBlock(LLVMBasicBlockNode materializedBlock) {
-            super(LLVMStatementNode.NO_STATEMENTS, new LLVMUnreachableNode(), materializedBlock.getBlockId(), materializedBlock.getBlockName());
-            this.materializedBlock = materializedBlock;
+        private LazyBlock(LLVMStatementNode[] statements, LLVMControlFlowNode termInstruction, int blockId, String blockName) {
+            super(blockId, blockName);
+            this.statements = statements;
+            this.termInstruction = termInstruction;
         }
 
         @Override
         public LLVMBasicBlockNode initialize() {
             CompilerDirectives.transferToInterpreterAndInvalidate();
+            final LLVMBasicBlockNode materializedBlock = new InitializedBlock(statements, termInstruction, getBlockId(), getBlockName());
             replace(materializedBlock, "Lazily Inserting LLVM Basic Block");
             notifyInserted(materializedBlock);
             return materializedBlock;
+        }
+
+        @Override
+        public void execute(VirtualFrame frame) {
+            CompilerDirectives.transferToInterpreter();
+            throw new IllegalStateException("Lazy block should have been materialized");
+        }
+
+        @Override
+        public LLVMControlFlowNode getTerminatingInstruction() {
+            return termInstruction;
+        }
+
+        @Override
+        public double getBranchProbability(int successorIndex) {
+            return 0;
+        }
+
+        @Override
+        public void increaseBranchProbability(int successorIndex) {
+        }
+
+        @Override
+        public String toString() {
+            CompilerAsserts.neverPartOfCompilation();
+            return String.format("uninitialized basic block %s (#statements: %s, terminating instruction: %s)", getBlockId(), statements.length, termInstruction);
         }
     }
 }
