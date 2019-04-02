e3c4a84bcedac9eae288c37bace448d4f748a732
Jacob Kreindl <jacob.kreindl@jku.at>
2019-04-02 18:45:54 +0200

    Propagate VirtualFrame for Instrumentation

--- sulong/projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/intrinsics/llvm/LLVMMemMove.java
+++ sulong/projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/intrinsics/llvm/LLVMMemMove.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2018, Oracle and/or its affiliates.
+ * Copyright (c) 2016, 2019, Oracle and/or its affiliates.
  *
  * All rights reserved.
  *
@@ -31,6 +31,7 @@ package com.oracle.truffle.llvm.nodes.intrinsics.llvm;
 
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.llvm.runtime.memory.LLVMMemMoveNode;
 import com.oracle.truffle.llvm.runtime.nodes.api.LLVMExpressionNode;
 import com.oracle.truffle.llvm.runtime.pointer.LLVMPointer;
@@ -51,8 +52,8 @@ public abstract class LLVMMemMove {
 
         @SuppressWarnings("unused")
         @Specialization
-        protected Object doVoid(LLVMPointer dest, LLVMPointer source, long length, boolean isVolatile) {
-            memMove.executeWithTarget(dest, source, length);
+        protected Object doVoid(VirtualFrame frame, LLVMPointer dest, LLVMPointer source, long length, boolean isVolatile) {
+            memMove.executeWithTarget(frame, dest, source, length);
             return null;
         }
     }
--- sulong/projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/memory/literal/LLVMStructArrayLiteralNode.java
+++ sulong/projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/memory/literal/LLVMStructArrayLiteralNode.java
@@ -60,7 +60,7 @@ public abstract class LLVMStructArrayLiteralNode extends LLVMExpressionNode {
         for (int i = 0; i < values.length; i++) {
             try {
                 LLVMNativePointer currentValue = values[i].executeLLVMNativePointer(frame);
-                memMove.executeWithTarget(LLVMNativePointer.create(currentPtr), currentValue, stride);
+                memMove.executeWithTarget(frame, LLVMNativePointer.create(currentPtr), currentValue, stride);
                 currentPtr += stride;
             } catch (UnexpectedResultException e) {
                 CompilerDirectives.transferToInterpreter();
@@ -80,7 +80,7 @@ public abstract class LLVMStructArrayLiteralNode extends LLVMExpressionNode {
         LLVMManagedPointer currentPtr = addr;
         for (int i = 0; i < values.length; i++) {
             Object currentValue = values[i].executeGeneric(frame);
-            memMove.executeWithTarget(currentPtr, currentValue, stride);
+            memMove.executeWithTarget(frame, currentPtr, currentValue, stride);
             currentPtr = currentPtr.increment(stride);
         }
         return addr;
