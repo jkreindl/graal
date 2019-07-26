27549d0dfc702a0bb2b06d1d2a7aa3667f123afc
Jacob Kreindl <jacob.kreindl@jku.at>
2019-07-26 15:07:17 +0200

    Remove Fields for Explicitly Stored Source Location from Nodes

--- sulong/projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/memory/LLVMGetStackSpaceInstruction.java
+++ sulong/projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/memory/LLVMGetStackSpaceInstruction.java
@@ -31,19 +31,29 @@ package com.oracle.truffle.llvm.nodes.memory;
 
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
+import com.oracle.truffle.api.TruffleLanguage.ContextReference;
 import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.CachedContext;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.NodeField;
 import com.oracle.truffle.api.dsl.NodeFields;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.FrameSlot;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.instrumentation.InstrumentableNode;
+import com.oracle.truffle.api.instrumentation.Tag;
+import com.oracle.truffle.llvm.runtime.LLVMContext;
+import com.oracle.truffle.llvm.runtime.LLVMLanguage;
+import com.oracle.truffle.llvm.runtime.instrumentation.LLVMTags;
 import com.oracle.truffle.llvm.runtime.memory.LLVMMemory;
 import com.oracle.truffle.llvm.runtime.memory.LLVMStack;
 import com.oracle.truffle.llvm.runtime.memory.LLVMStack.UniquesRegion.UniqueSlot;
 import com.oracle.truffle.llvm.runtime.nodes.api.LLVMExpressionNode;
 import com.oracle.truffle.llvm.runtime.pointer.LLVMNativePointer;
+import com.oracle.truffle.llvm.runtime.types.PrimitiveType;
 import com.oracle.truffle.llvm.runtime.types.Type;
+
+import java.util.Set;
 
 @NodeFields({@NodeField(type = int.class, name = "size"), @NodeField(type = int.class, name = "alignment"), @NodeField(type = Type.class, name = "symbolType")})
 public abstract class LLVMGetStackSpaceInstruction extends LLVMExpressionNode {
@@ -97,6 +107,51 @@ public abstract class LLVMGetStackSpaceInstruction extends LLVMExpressionNode {
         protected LLVMNativePointer doOp(VirtualFrame frame,
                         @Cached("getLLVMMemory()") LLVMMemory memory) {
             return LLVMNativePointer.create(LLVMStack.allocateStackMemory(frame, memory, getStackPointerSlot(), getSize(), getAlignment()));
+        }
+
+        @Override
+        public InstrumentableNode materializeInstrumentableNodes(Set<Class<? extends Tag>> materializedTags) {
+            // in this node the size to be allocated already contains the number of items of the
+            // given type to allocate, we replace this node with one that still uses the same size
+            // to allocate, but also executes a child for the count, so the instrumentation is
+            // notified of the count
+            CompilerDirectives.transferToInterpreterAndInvalidate();
+            final MaterializedAllocaConstInstruction materializedNode = LLVMGetStackSpaceInstructionFactory.MaterializedAllocaConstInstructionNodeGen.create(getSize(), getAlignment(),
+                            getSymbolType());
+            materializedNode.setTypes(getTypes());
+            materializedNode.setOffsets(getOffsets());
+            materializedNode.setSourceDescriptor(this.getSourceDescriptor());
+            return materializedNode;
+        }
+    }
+
+    public abstract static class MaterializedAllocaConstInstruction extends LLVMGetStackForConstInstruction {
+
+        // initialized at runtime since its creation requires a context for computing type sizes
+        @Child private LLVMExpressionNode countNode = null;
+
+        @Specialization
+        protected LLVMNativePointer doOp(VirtualFrame frame, @Cached("getLLVMMemory()") LLVMMemory memory, @CachedContext(LLVMLanguage.class) ContextReference<LLVMContext> contextReference) {
+            if (countNode == null) {
+                CompilerDirectives.transferToInterpreterAndInvalidate();
+                countNode = insert(createCountNode(contextReference));
+                notifyInserted(countNode);
+            }
+
+            // the actual value of the count is ignored, it is only produced so the instrumentation
+            // framework receives a value and reports the input
+            countNode.executeGeneric(frame);
+
+            return LLVMNativePointer.create(LLVMStack.allocateStackMemory(frame, memory, getStackPointerSlot(), getSize(), getAlignment()));
+        }
+
+        private LLVMExpressionNode createCountNode(ContextReference<LLVMContext> contextReference) {
+            final Type typeToAllocate = getSymbolType();
+            final int typeSize = contextReference.get().getByteSize(typeToAllocate);
+            final int count = getSize() / typeSize;
+            final LLVMExpressionNode newCountNode = getNodeFactory().createSimpleConstantNoArray(count, PrimitiveType.I32);
+            newCountNode.getOrCreateSourceDescriptor().setTags(LLVMTags.Literal.EXPRESSION_TAGS);
+            return newCountNode;
         }
     }
 
